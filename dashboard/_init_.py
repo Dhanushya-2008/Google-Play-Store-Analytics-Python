60c5b7c4530965eb3bef3dbfbb98b4cdd82744ee
Dhanushya-2008 <sfe25am020@hcctrichy.ac.in>
2026-02-15 01:04:16 +0530

    Create _init_.py

---
+++ dashboard/_init_.py
@@ -0,0 +1,17 @@
+from dashboard.Task 1 import mobile_category_polished_chart
+mobile_category_polished_chart()
+
+from dashboard.Task 2 import free_vs_paid_chart
+free_vs_paid_chart()
+
+from dashboard.Task 3 import global_installs_choropleth
+global_installs_choropleth()
+
+from dashboard.Task 4 import cumulative_installs_chart_time
+cumulative_installs_chart_time()
+
+from dashboard.Task 5 import task5_bubble_chart
+task5_bubble_chart()
+
+from dashboard.Task 6 importtask6_trend_chart
+task6_trend_chart()
